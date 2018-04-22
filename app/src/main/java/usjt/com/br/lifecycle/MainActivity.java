35919229edb6eaccd3d6cbc255e8a90bb5928b37
Marcelo victor <marcelo.victor525@gmail.com>
2018-04-22 02:03:30 -0300

    Initial commit

---
+++ app/src/main/java/usjt/com/br/lifecycle/MainActivity.java
@@ -0,0 +1,19 @@
+package usjt.com.br.lifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Marcelo victor da Silva
+ * RA: 816119006
+ * ADSMCA3
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
